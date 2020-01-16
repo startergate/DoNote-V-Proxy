4de5b98892b46faa89eed6b79b6f008c4fc6d2ed
startergate <startergate36@gmail.com>
2020-01-16 14:54:40 +0900

    [CREATE] Reverse Proxy
    copy-pasted from https://www.integralist.co.uk/posts/golang-reverse-proxy/

--- app.go
+++ app.go
@@ -1,8 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
+
 func main() {
-	fmt.Println("hello world")
+	origin, _ := url.Parse("http://localhost:9000/")
+
+	director := func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", origin.Host)
+		req.URL.Scheme = "http"
+		req.URL.Host = origin.Host
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":9001", nil))
 }
